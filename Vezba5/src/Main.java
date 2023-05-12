f03f258695a8bd4bb6823b54147b75f3c327139e
AlwaysDita <132491022+AlwaysDita@users.noreply.github.com>
2023-05-12 17:34:29 +0200

    Add files via upload

---
+++ Vezba5/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int operand1 = 2;
+        int operand2 = 3;
+        int sum = operand1+operand2;
+        System.out.println(sum);
+        /**
+         * Ovom programu je zadatak da izvrsi operaciju sabiranja dva broja
+         * int operand1 je vrednost prvog broja u ovom slucaju "2"
+         * int operand2 je vrednost drugog broja u ovom slucaju "3"
+         * int sum je rezultat koji ce program da izvrsi u konzoli tj sabiranje vrednosti zadatih brojeva "2" i "3"
+         * kao metodu konzola koristi System.out.println(sum) da bi prikazala krajnji razultat
+         */
+    }
+}
